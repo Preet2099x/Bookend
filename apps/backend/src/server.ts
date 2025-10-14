625ac052ada931a83f97093ec0b3b90e7a063e6e
Preet2099x <preet2099x@gmail.com>
2025-10-14 23:59:13 +0530

    changed messages

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -5,9 +5,9 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.get('/',(req:Request,res:Response) => {
-    res.status(200).json({message: 'Behold the Backend!'});
+    res.status(200).json({message: 'You have successfully tickled the mainframe. It is pleased!'}); // need here
 });
 
 app.listen(PORT,() => {
-    console.log(`Server is running on on http://localhost:${PORT}`);
+    console.log(`Releasing the kraken on http://localhost:${PORT}`); //need here
 })
